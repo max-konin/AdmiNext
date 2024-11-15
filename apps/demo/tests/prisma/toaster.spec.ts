cb2e4cfced40e4f7a0c9c550b43da251030d8c83
Julia Lepner <ylepner@gmail.com>
2024-11-15 12:09:49 +0100

    update playwright tests

--- apps/demo/tests/prisma/toaster.spec.ts
+++ apps/demo/tests/prisma/toaster.spec.ts
@@ -2,45 +2,41 @@ import test, { expect, Page } from '@playwright/test';
 import { prisma } from '../../app/db';
 
 test('"New" record successful notification', async ({ page }) => {
-  await page.goto('/admin/categories/new');
-  await page.getByRole('textbox').fill('New Success Test Record');
-  await page.getByRole('button', { name: 'Submit' }).click();
-  await expect(page.getByText('Loading...')).toBeVisible();
+  await createNewRecord(page, 'New Successful Test Record');
+  await expect(page.getByText('Saving...')).toBeVisible();
   await expect(page.getByText('Done!')).toBeVisible();
   await prisma.category.deleteMany();
 });
 
 test('"New" record failed notification', async ({ page }) => {
-  await page.goto('/admin/categories/new');
-  await page.getByRole('textbox').fill('New Failed Test Record');
-  await page.route('**/*', async route => {
-    await route.abort();
-  });
-  await page.getByRole('button', { name: 'Submit' }).click();
-  await expect(page.getByText('Failed to add new record')).toBeVisible();
+  await prisma.category.deleteMany();
+  await createNewRecord(page, 'New Test Record');
+  await expect(page.getByText('New Test Record')).toBeVisible();
+  await createNewRecord(page, 'New Test Record');
+  await checkToast(page, 'Failed to add new record');
+  await prisma.category.deleteMany();
 });
 
 test('"Edit" record successful notification', async ({ page }) => {
   await prisma.category.deleteMany();
-  await createNewRecord(page, 'New Success Test Record');
-  const id = await getRecordId(page, 'New Success Test Record');
-  await page.goto(`/admin/categories/${id}`);
+  await createNewRecord(page, 'New Successful Test Record');
+  await page.getByTestId('resource-table-row').getByRole('button').click();
+  await page.getByRole('menuitem', { name: 'Edit' }).click();
   await page.getByRole('button', { name: 'Submit' }).click();
-  await expect(page.getByText('Loading...')).toBeVisible();
-  await expect(page.getByText('Done!')).toBeVisible();
+  await checkToast(page, 'Done!');
   await prisma.category.deleteMany();
 });
 
 test('"Edit" record failed notification', async ({ page }) => {
   await prisma.category.deleteMany();
   await createNewRecord(page, 'New Failed Test Record');
-  const id = await getRecordId(page, 'New Failed Test Record');
-  await page.goto(`/admin/categories/${id}`);
-  await page.route('**/*', async route => {
-    await route.abort();
-  });
+  await createNewRecord(page, 'New');
+  await page.getByTestId('resource-table-row').first().getByRole('button').click();
+  await page.getByRole('menuitem', { name: 'Edit' }).click();
+  await page.getByRole('textbox').clear();
+  await page.getByRole('textbox').fill('New');
   await page.getByRole('button', { name: 'Submit' }).click();
-  await expect(page.getByText('Failed to update record')).toBeVisible();
+  await checkToast(page, 'Failed to update record');
   await prisma.category.deleteMany();
 });
 
@@ -50,8 +46,7 @@ async function createNewRecord(page: Page, recordName: string) {
   await page.getByRole('button', { name: 'Submit' }).click();
 }
 
-async function getRecordId(page: Page, recordName: string) {
-  await page.goto('/admin/categories');
-  await expect(page.getByText(recordName)).toBeVisible();
-  return await page.getByTestId('resource-table-cell__id').first().textContent();
+async function checkToast(page: Page, message: string) {
+  await expect(page.getByText('Saving...')).toBeVisible();
+  await expect(page.getByText(message)).toBeVisible();
 }
