337266849e4fab1f44c1c15d2b0267a7eb81e9fa
Julia Lepner <ylepner@gmail.com>
2024-12-11 14:35:03 +0100

    add filterByType test

--- packages/core/src/utils/filters.ts
+++ packages/core/src/utils/filters.ts
@@ -6,6 +6,9 @@ export const filterByType = <
 >(type: string, fieldName?: string) => {
   return type === 'object'
     ? (obj: Row<TListData>, field: TListFields, search: string) => {
+      if (!fieldName) {
+        return false;
+      }
       const value = obj.getValue(field) as { [key: string]: any };
       return value[fieldName!].includes(search);
     }
