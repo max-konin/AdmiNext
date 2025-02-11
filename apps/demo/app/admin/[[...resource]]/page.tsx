267ff9b46dc2332e22d626bf5bf0d7a9bd45034f
Max Konin <maxim21214@gmail.com>
2025-02-11 19:41:40 -0300

    docs

--- apps/demo/app/admin/[[...resource]]/page.tsx
+++ apps/demo/app/admin/[[...resource]]/page.tsx
@@ -1,12 +1,18 @@
 import { DataProvider } from '@adminext/core/server';
 import { RouteProps } from '@adminext/core';
 import { adminResources } from './model';
-import { AdminPanel } from './admin-panel';
+import { AdmiNext } from '@adminext/core/client';
 
 export default async function Home(props: RouteProps) {
   return (
     <DataProvider resources={adminResources} routeProps={props}>
-      {(data) => <AdminPanel {...data} />}
+      {(data) => (
+        <AdmiNext
+          resourcesDefinition={adminResources}
+          routePrefix="admin"
+          {...data}
+        />
+      )}
     </DataProvider>
   );
 }
---
+++ apps/docs/mdx-components.ts
@@ -0,0 +1,10 @@
+import { useMDXComponents as getThemeComponents } from 'nextra-theme-docs'; // nextra-theme-blog or your custom theme
+
+// Get the default MDX components
+const themeComponents = getThemeComponents();
+
+// Merge components
+export const useMDXComponents: typeof getThemeComponents = (components) => ({
+  ...themeComponents,
+  ...components,
+});
