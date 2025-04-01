2a4b3e8bcd3b7aba46c986ea8d99b3fb63dc2263
Julia Lepner <ylepner@gmail.com>
2025-04-01 18:09:19 +0200

    Replace react icons with svg (#20)
    * replace icons in demo
    
    * replace icons in core

---
+++ packages/core/src/components/icons/ListFilterIcon.tsx
@@ -0,0 +1,20 @@
+export const ListFilterIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14px"
+      height="14px"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="lucide lucide-list-filter-icon lucide-list-filter"
+    >
+      <path d="M3 6h18" />
+      <path d="M7 12h10" />
+      <path d="M10 18h4" />
+    </svg>
+  );
+};
